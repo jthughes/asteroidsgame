c29f7cca80f7d506397770bc84f78d5f10ce62a8
Jeremy Hughes <jthughes@users.noreply.github.com>
2024-12-04 11:33:45 +1030

    Added typehints, rendering a player object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 
 def main():
@@ -10,15 +10,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        loop(screen)
+        loop(screen, player)
+        dt = clock.tick(60) / 1000
 
-def loop(screen: pygame.Surface):
-    pygame.Surface.fill(screen, pygame.Color("black"))
+def loop(screen: pygame.Surface, player: Player):
+    screen.fill("black")
+    player.draw(screen)
+
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
